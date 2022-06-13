67172ba7487d5fb14c182cb7795832be71c32e3a
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-06-13 17:50:54 +0000

    added scoreboard, increase ball speed with every hit of the paddle

--- ball.py
+++ ball.py
@@ -1,26 +1,29 @@
 from turtle import Turtle
-import random
 
-rand_num = random.randint(-10, 10)
 
 class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
 
-  def __init__(self):
-    super().__init__()
-    self.shape("circle")
-    self.color("white")
-    self.penup()
-    self.x_move = rand_num
-    self.y_move = rand_num
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
 
-  def move(self):
-    new_x = self.xcor() + self.x_move  
-    new_y = self.ycor() + self.y_move
-    self.goto(new_x, new_y)
+    def bounce(self):
+        self.y_move *= -1
 
-  def bounce(self):
-    self.y_move *= -1
+    def hit(self):
+        self.x_move *= -1
+        self.move_speed * 0.9
 
-  def hit(self):
-    self.x_move *= -1
-    
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.hit()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -21,17 +23,27 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
-  time.sleep(0.1)
-  screen.update()
-  ball.move()
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
 
-  # Detect collision with wall
-  if ball.ycor() > 280 or ball.ycor() < -280:
-    ball.bounce()
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
-  # Detect collision with paddles
-  if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
-    ball.hit()
+    # Detect collision with paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(
+            l_paddle) < 50 and ball.xcor() < -320:
+        ball.hit()
 
+    # Detect if ball misses r_paddle
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect if ball l_paddle
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
